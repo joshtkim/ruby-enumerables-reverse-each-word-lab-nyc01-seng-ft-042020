f325ebe662953f191d7ae1c7eeaaca409d7f4fd4
Joshua Kim <jykim92@gmail.com>
2020-04-10 16:24:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,9 @@
 def reverse_each_word(string)
-  string.split.map.join do |word|
-    word.reverse
+  new_string = []
+  string.split.each do |word|
+    new_string = word.reverse
   end
+  new_string.join
 end
 
 reverse_each_word("Hello this is an example.")
